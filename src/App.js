f82ad38deb56595f77fa1b409393936d0a47b016
AlbertMrMaruk <albertmarukyan@yandex.ru>
2024-01-09 23:07:37 +0300

    New commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { cards } from "./card";
+import ReactHtmlParser from "react-html-parser";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+function App() {
+  const [currentCard, setCurrentCard] = useState({ id: 0, expand: false });
+
+  return (
+    <div className="w-full bg-[#0a092d] h-[100vh]">
+      <h4 className="font-bold text-2xl text-white m-auto text-center pt-8">
+        {currentCard.id + 1} / 40
+      </h4>
+
+      {cards.map((el, index) => (
+        <div
+          className={`bg-[#2e3856]  rounded-lg w-[70%] m-auto my-5 p-0 h-[70vh] ${
+            currentCard.id === index ? "" : "hidden"
+          }`}
+          onClick={() =>
+            setCurrentCard((prev) => ({ id: prev.id, expand: !prev.expand }))
+          }
+        >
+          <div
+            className={`w-[100%] p-2 m-auto ${
+              currentCard.id === index && !currentCard.expand ? "" : "hidden"
+            }`}
+          >
+            <img src={el.img} className="m-auto w-[100%]" />
+          </div>
+          <div
+            className={`w-[100%] p-5 m-auto text-white font-bold text-xl text-center ${
+              currentCard.id === index && currentCard.expand ? "" : "hidden"
+            }`}
+          >
+            {ReactHtmlParser(el.text)}
+          </div>
+        </div>
+      ))}
+
+      <div className="flex gap-6 m-auto w-[100%] justify-center">
+        <div
+          className="rounded-full p-4 bg-[#2e3856] cursor-pointer"
+          onClick={() => {
+            if (currentCard.id === 0) {
+              setCurrentCard({ id: 39, expand: false });
+            } else {
+              setCurrentCard((prev) => ({ id: prev.id - 1, expand: false }));
+            }
+          }}
+        >
+          <FaArrowLeft className="text-white font-bold text-2xl" />
+        </div>
+        <div
+          className="rounded-full p-4 bg-[#2e3856] cursor-pointer"
+          onClick={() => {
+            if (currentCard.id === 39) {
+              setCurrentCard({ id: 0, expand: false });
+            } else {
+              setCurrentCard((prev) => ({ id: prev.id + 1, expand: false }));
+            }
+          }}
+        >
+          <FaArrowRight className="text-white font-bold text-2xl" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
