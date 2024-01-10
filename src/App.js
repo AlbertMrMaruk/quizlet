18aba1e5659a865acc640958cdf3e29cbe4554cc
AlbertMrMaruk <albertmarukyan@yandex.ru>
2024-01-10 12:04:05 +0300

    ggd

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
       {cards.map((el, index) => (
         <div
-          className={`bg-[#2e3856]  rounded-lg md:w-[70%] m-auto my-8 md:my-5 p-0 md:h-[70vh] min-h-[39vh] w-[95%] ${
+          className={`bg-[#2e3856]  rounded-lg md:w-[70%] m-auto my-8 md:my-5 p-0 md:h-[70vh] min-h-[42vh] w-[95%] ${
             currentCard.id === index ? "" : "hidden"
           }`}
           onClick={() =>
@@ -22,7 +22,7 @@ function App() {
           }
         >
           <div
-            className={`w-[100%] md:h-[100%] h-[39vh] p-2 ${
+            className={`w-[100%] md:h-[100%] h-[42vh] p-2 ${
               currentCard.id === index && !currentCard.expand
                 ? "flex"
                 : "hidden"
@@ -31,7 +31,7 @@ function App() {
             <img src={el.img} alt={el.img} className=" m-0 w-[100%]" />
           </div>
           <div
-            className={`w-[100%] p-5 m-auto text-white font-bold  text-sm  md:text-3xl md:py-[4rem] md:px-[2rem] md:text-center h-[39vh] md:h-[100%] ${
+            className={`w-[100%] p-5 m-auto text-white font-bold  text-sm  md:text-3xl md:py-[4rem] md:px-[2rem] md:text-center h-[42vh] md:h-[100%] ${
               currentCard.id === index && currentCard.expand
                 ? "flex items-center"
                 : "hidden"
